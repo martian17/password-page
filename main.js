a2b09d923fba32f5f777eaa7e3763112d04625e3
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-10-17 15:14:48 +0900

    password page

---
+++ main.js
@@ -0,0 +1,87 @@
+let encrypted = "U2FsdGVkX19B8YI+m1PC8iXhS3RkaYYPrZJCnbpqVx4wkbZRtvSJc0vfdiheh5iHfu/p9YfAxCk176XvegOpnBhXHL+IakjEDorA7i3UdrZ/XvSGYFHmRCx8Tv9jJuCnORZHwYMv0Q50NLKCvUufObfqWbW4YEIhbR+C+ooCv+ox57ytUsoXgfigmA+DXnAjC2I7DuLiF8YJk5uh4Wedf+hTn/KzSHVpeJVDNRHTD3+26AvDe8JtbkMpdI9U6XMSh04Kyvt4bPdTKafAmkso+NoHI599cTD2x6YFyjLHPjC48qgX6vEQVMYZVIlm5ENuxhntXCkSlLiOtqS2hRFwhiTfGwxWMDncVWrtX58CJeggF570l8rXpzlUI3VJBtuLsO5a7gn5HyB/cbMIayjMB7ZTU1yY3nd8aNRkFcyo8FsUWlQcFXi1/LVi9lOVCp2GAzJjegM3sW6iqjTUuws6YGjsEWUzw4umgE+0PxwC3YOEB7muyhF7Q1i9MjKyaxlPk5d/Vemk/1G9N0KH40NheUVWcLl+Q6wa/WNdDCWuOJHkbMikcFHM8NQZdaE318s5IbK2QLBUEJAiRAr9iI3eXnx4AxfhquAH24DJD/PPjzp4tdQW/NHEBjGmmQyREHm85E0tv8lW6lTSs4LBJBrgA5RvD5utuLqWQcWWciRKGtkvtZnq9RbXfP/wPsb0Sl0rUm4NWuTbcEOaXtTP7YcBPgwIks9RQc7CY9QlcGrxS4kk9NObfGRvfAFiSr/HoW6aaE/NVridWUph8LwcH6GYyf7/vkU8aYeW+Z8MRC8ZYK8XKR4PxEUvoyzLcvyDT8Rj8QE1l0Ed4an9D6NpgYqxzGsh6zET7cu2dNu8zTJQyCVQl97Zxj1hxRh/mkyq4QmiYSWlZiI/x9Fr7NtnvXPnhKqfsxCWDOLhgSBfdGi9gMtx5/VcPp6Nphv3urAoSQ3NBHyMaNpUW9YCAciQJzGVRm5Bb6Q/o0EEMFHjfV5i/nI4LYiUZyggSIASRNlhpjb5I/Vd4eWumwxWD/2VgmcuS6s6aj+2fdhpAjhXFHu6JZoh4t4NW0nMqaN1OrSXtZ1IZ/o2zTFBudXvLSwOSzFLf++SUP5HOg/h5itVsoBioqzKe9iICyRUG/DVZqCC7KhX+Lc1EYG4N00oiJk4NvBdywJEBuJ/nhvOlGHfHE5f/EkB+U/RvrKZ9/Loy23Xk/78LzhDA/npAZ3zHOqG1T1FJ+7NOC+FxBydlumbXAEJy+CsmWy+m/tnMASnCihkDPwn8/unlchNgM9bOSBLdRexC/z6CXmdPM68SmPPiCsykBLhFDFMhGmw57emiE2J4e1wuR2zbwThpw66mGnkJ/CwZN84hI3I+499OsQ+oPXdB9bqF4RJ3KJtRybeeSNR7YWldRQ9xENiQLHLCB/NcAS9n50nSTv7P0F9IGH5X0J4AbJmqf+ehtYBKwBZI3aW8LpaASIHIYkws2vZC2qQ5TvJOIcekHho5g1aL2xdmAVb2Xk3G6y+Yv/xvH71TjuPrLHBqiWSFiSTzI33NleQw3Gqq64jiaLI5jZ3cc9TxT/aRmebqHBFi5KJ3QVFQiXn+wxL6gGiysHNC01phM0I1kVT5tcJOytSRkuuD4PjgREdZUsA6auXN8lRtcDVA9hbJ08vwACLQeI6O4WyChKQN4/thlnU9fJ0o8ulOk9pewDC/xEuRKINSwkT+bADR+ZP4SqZaHdUj5epvHvH93dUKuO6blZDJF9z85IsAPl3UcLcVmcXtUCq3aYs6MVwxdixI3ExmQajFpdvyp0cwHkkGMRGJT4BBSPFRufMlAFH5UniF7We9IgGxvvBsI0iNUZqcJa3vnZXPMR7aKSf4uD53EZrtU3CSWza4mTMXvN9K046A5Kdy61RyG6gPyObz35TEsasT/R+ikctv2++TM4ELedw7p5jw6Z/fDzlEeUwq4h74cArfN0JWro+yfOGQ7o9Y5EQCLDl0bm+iWLpyTJaSoabI0YU3Ob4qgjFAtW7A5mPY+bGu65qbHvsDHyksi7KuJmu4LJlHWzU8DsUam+QjyKTCTdDGAkVpmIzZYPyuW2HcYkYdIAeTxGwCX1iDcZXlFHd1PmiL8QnFuaMyR3XoV9DPINzSBUYg9LrgmsoYUEDh0wbHXAqmd5pXMu5inKrveojIUubzX1ITVtFz/AYjDT1jXCXVIL8ACnfBriQsHXnfxsrI8KUofSa3qdSSeTaD9fQYGVBPQ41z85VuJN4GhUyJ8r0bSuOuYWQcemda4UrIpzPD6hJlcBZR4QxpUsfFjTUuhoSkPEY1mHl04D53iy+sA2jRzfJ3lW0WeHcM0IeIc4+/Ws9oymWua2aZtcyggv2OpCcxmTSc9nJWcsDRX47+2TgXSKoPTzeOF4Blg8f3m1XqSwccQEBgJdyD4gbhwa6qbAxytCyuafjU8eWsOL5J+pKPZ/UYK//tFwMzaxP51MiuXYB+l/mRo78Gy5ssEgeWVPWdirrfQf5CqmzreRWqlWlMYzZnZg5yuNGZAtRj9GIk2PmU/wDGVHmZtWFidW4e2o2kjbEpUO0zw6oxz1qgA9IoGUMdAOn2hWJ2DkQgsgf9zsmmXGKkTNZ/5IQCbyDAgpQFhm8KDwmN0Sptp8i7qxcN4qZ6TDwZ24Rji8GSJrzcnE9A4Gv2M6Z9i/kLFKc9kpcdR8c3lD9xk+Nk7xyidSFx/71hQqeLcwNawpY9RYpcUxOCLbAp2FBwzuKVRji9hyf4vIU/d8upM1OG5wWRhsDpIam4iMM7KGkgb+bASPr5smERDnQAFCZ5c/ceJ+Pc/oLeUK+VotDqTdOWbpiCxPc6C4evEIlbS4furgIeL6+yhAmQ8LSsT8cbgSBt+xlyHY4IvGUQui1AUuN3Wnn9DNABfcveMXoW/whJrZ2/zWdtqCM5hgfRrFtDLi9MALGQbbXrpCGkT4QuwjEjpQpiFPMgxQdgODUcHUzmh3a1sQnqYYL2zyhlhoUBWtgqtdEYor1kld3p4cNUfmp8U1WfaoxuOwPkEP6uJCiZH4RMaSiQAvT66HNfBT05zIBxPy0az8Rs1yAU8aBj9jijFpmAdG3ye8146B0kTqc74TkRycSMV1XlLk5nXTBELN8atZZeMthzYKukJGWvMFcob9EAAikwuJUi1qIwOTePA1H5U8e64SdmI5XtFs8DxDkbJQ+aGLxJRnku3D8F2O7in3UbUY3glCO/vxumLgRPrJrMy24JMem6uFG4NfJxRl2SY81ALOctc28641uYqn6e6AI+yrpF9MgXOjZffqm9zW1LrS9k4GV+o3NTTG6Mk1+g+SyS6oMLzYv8Exil0TeDcPuv1dwG69bn2+Bkj8GSPjIqqVCmmd76vCajzWDOWkW5EbHoYHZ1qKd7YVcKp34991Q7L5MWjyxEGYZzzJkuDNyDOfpoqwrlwy/XGM3dIqsmFP9mXemQ6Oof5wDdZJCLdqIzAw0DoBffqpeGnYO3uvCV10eK5e58p2fhqsdLic6TGn2Yz5hBl6MJGO99PaX5R9PSjBTi8pMsrqFXCmXkHJT9HZ6uQttgjFfzB04557udSCn7lgG7AU2bjnDSNR7xE3DmMD1Z5Qsfi/UCHzqD8bZYZi2/P/CN8p0MNJEaNctkY/x72UBB913d9ZDlT6U7u1+Ay/3x5KbOiC5Y0mrRXu8Mb1yBcv5kUOvQzNdVcyweoHaHVuxysaJwCDLXA8zBD6sCLtebSrWoE75EA3X5LlSgaESKtEcX6K/YWGYdkkUtbcWgZTLsIzL/F1r1ekOx8H6Tt4DFqV2Fdh2SOD1iTHxlzm3HawDbqoWkuasyVHyUGAdCuSGOZgFd1dD/zVa4wZQUKY2QeilvQeJfga5NuuavalXHY0lSO5nmfFMLMgh88HAe0OAt+l/ZwujoidaZfIndkyLcPDjB5yJvqq5a8ep+FlImVFXkBQ0WC+Z7pdbf8xtfxA9/dbixGje7HlknAG6QKs7Dr2uEfZzyj9xgE9+YcVCdaJUBzIu70n1m7ZgqkGhOZzm/1Ida4BZyS7xwgzVmafsuqSnfD2PCV+OEUcb6wJlH3vNNZo1Ma2RbH66t3Bc+uA4HOP7KdxOCmh/fpO/sm2iz1cYKsIW58/sRPTMarIYy8UiRxeXIrXchmAg9DU5EHn/OsNzCmUKfL7Yjue/FGqDZ07BG8JDy7dzDvZv6K1butHfvZViR9wjBnvlkblvy+0w6jWk2hO5RNAlx5IZLYJmIbB561DL64WgHofh1LGTeMXCJ3RdjM+EPJF5Lk7AymF93hL6GPJyCYfm3Ysbb12viZ9hKwUOaZR3KB3tC+4pzc+g+ZdRNzfZ364KUaTrJo6L5PRdJXmxkGrZF6Bxfz2gmUBCvs9ipXeICksH0bwtcOCH9opze2K0ty+ZfHgKE9/hmqEI8sm2kfC7IdZPTIokqil6wgtP8KvmmdoKgCtA9fpvIOZzvfdPP9gsuu5yvPwbC+tdXIJc6EepiyHuabnCmEnsSq4nYwyxfuxzZ+c2XrG6eUy8kGOOVDKHo/RWW1RtTx5ChzWUdy/+FR22rDou1nh7oA9LOmDvF/Jn9e0jEDlp5KM5/6lQtUfeqdREQ3qCOr5BmPEtjjLSf3vKUSgLK+27OcbENts193JQT6mckuDtlnpE8LbguSoqJxSIeL9I6tARxFYj2jWvIvAT1h6eXw3wpKxm2PKvMVQWDLnMupExOwAed9sz6N0Ny00kJOYh1+aoVff8zrLGFAjgoDESQfubY5bUvUPU9EkrNJsK6P+6SP01mBKQnWNfNOVQqkeq5RcXNxzOE27x4WK6ejE8ca8L3/Epl/S9o+KFeVooyQfP+8BdkLGHmpXTpZqy2kVdVZiT8UKmCDUtJxKM1cRlJk3bgmQAV6OwqRAI75z3UKGM3GEExmltK+Iw3RlFCCOTXA2qGEU9qsQ7kvJ4wp0gF5nSho4IuOnDKm3L3t4pafE0xv0m9fc2O6qb+S370eycb2AckWhrQBpmH/2R07aRuIbWzwGBwaO+Ah2p8WW3CvvvaEwrL3VzoxSFqJ2c90S6OZ6hdQ6f32yIRG2XF6ZEO7DaiAF0ZUILIvtpuDsgJPN0wNC9V8zY4/MfS8uEtc8QhlCyjBFtymrb4aCOA4rambR9YDWMneRAYn//pBh/EcBkJVkm6dPwCaYdFdukrrN++vhKsd5J77xd2Hie88lwfHLSfJoGw9IXiQEioVpvWD/UpO2348HSSQjcKrlUUdO/J8lRwwH4ji+0VjhFlJIbBtKt/hlXnXAanPF6nd41aEb/8ocuW+emrUrJ39AOS6wz5RfPcrOD/DQARE/K/LlhFqs+Iad5NaD4CEAIV4l5hGRxT8l+s9Plwd+PVUEEUK/ru2MItyCRTes67EOe8uRne5NTCm2i8Obefojth+A2xphsKqoO1bJ9EUMGNYTIrGKKwhsZzSqaVVhjwPy7lC24oiiAsUVxDupPhYVFqYNKmHmqgmoEqyTumy35LEBxXm3ds2AWKSpMac6sNT3AEZywtnJe/MW+PWEB8nN/yNfOBNPTwRmpHNBjCU9RPTc8hn5VGMsBnULXPj+aqcIt7K1lAFjRzqYBBXnDCKpYFpBTLz6xQvZb1n5mhHNlpRrLnGe9taVO7cdIBK2+nH38eiJzrMSwsHVhEw5Uhaw1YYDzDp0PpTTZtL4OVdOHrxM0HueDsU5bcSL0o7RTNqm/pWIeiCeVMJ/5FPX9jzXlV1KilAPNdPNw7WbAFe1FbLUZSWdhWCE/0ng+4/I1hCIdFHqHGYh5hWtO64k89KCdR2lNLzs5UWsaK2kVfd7ucZgIWWcF2BoJonIlQGfGEwpx4GPanoIgxiXAHCItARnLLoBF8JsGNNua/4tZjeOzetA4skH4NpLcR8ibyoQj56Vi8gDwt3iIt4aQcMoxGK+d9iy+9uAqrraLbsQv65BespOUh62MojLFtcmbvjB75Qs5YyuscvRNA1pJEuaqMgrjRr2+UWmJFp4Xyv2KeOwo7v0cM+02paOdWlCxLaNaEwdaJTJ4VFoV4X+89GjUTAYhefDK9ksGgo50WDNy1waUvwVtwUa2WKIpud8ecylxuMGeg7thHBsBTcMhWyVSlWvFONESpS9RpzEIZaSq/Knnlska"
+
+
+//How to encrypt
+//CryptoJS.AES.encrypt(JSON.stringify({message:"message"}),pass).toString();
+
+
+let Pause = function(t){
+    return new Promise((res,rej)=>{
+        setTimeout(res,t);
+    });
+};
+
+let Popup = function(elem){
+    this.hidden = true;
+    elem.e.classList.add("popup");
+    elem.e.classList.add("invisible");
+    this.hide = async function(){
+        this.hidden = true;
+        elem.e.classList.remove("visible");
+        elem.e.classList.add("invisible");
+    };
+    this.show = async function(){
+        console.log(this.hidden);
+        if(!this.hidden){
+            elem.e.classList.remove("visible");
+            elem.e.classList.add("invisible");
+            await Pause(100);//wait 100 ms
+        }
+        this.hidden = false;
+        elem.e.classList.remove("invisible");
+        elem.e.classList.add("visible");
+    }
+};
+
+
+let main = function(){
+    let body = new ELEM(document.body);
+    
+    let wrapper = body.add("div","class:contentWrapper").add("div","class:wrapper");
+    wrapper.add("h2",false,"Password protected page");
+    wrapper.add("h3",false,"Please enter the password below to view the page content");
+    let inputArea = wrapper.add("div","class:inputArea");
+    let decryptSequence;
+    let listeners = [];
+    let input = inputArea.add("input","type:password;placeholder:Enter password here");
+    listeners.push(input.on("keydown",function(e){
+        if(e.key === "Enter"){
+            decryptSequence();
+        }
+    }));
+    listeners.push(inputArea.add("input","type:button;value:Enter").on("click",function(){
+        decryptSequence();
+    }));
+    inputArea.add("label",false,"hide password");
+    inputArea.add("input","type:checkbox;checked:true;").on("input",function(e){
+        if(this.checked){
+            input.e.setAttribute("type","password");
+        }else{
+            input.e.setAttribute("type","text");
+        }
+    });
+    
+    let accessDenied = new Popup(wrapper.add("div","class:accessDenied","Wrong passphrase: Access Denied"));
+    let accessGranted = new Popup(wrapper.add("div","class:accessGranted","Correct passphrase: Access Granted"));
+    
+    decryptSequence = function(){
+        let password = input.e.value;
+        let decrypted = CryptoJS.AES.decrypt(encrypted, password).toString(CryptoJS.enc.Utf8);
+        let obj;
+        try{
+            obj = JSON.parse(decrypted);
+        }catch(err){
+            accessDenied.show();
+            return false;
+        }
+        accessDenied.hide();
+        accessGranted.show();
+        //if no error, decrypt success, correct password
+        //remove listeners
+        listeners.map(l=>l.remove());
+        wrapper.add("h1",false,"Secret text");
+        wrapper.add("p",false,obj.message);
+    };
+};
+
+main();
